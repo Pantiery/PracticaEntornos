d825312e1e8404f7500b92b56b0ad5a8ad2143ad
Matias Pencef <matiaspencef@gmail.com>
2024-03-27 23:52:27 +0100

    Creado el JavaDoc con la version 1.0 del programa.

--- PracticaEntornosS_Santa/src/starwars/Jedi.java
+++ PracticaEntornosS_Santa/src/starwars/Jedi.java
@@ -1,7 +1,17 @@
 package starwars;
 
 import java.util.Objects;
+
 import java.util.Random;
+
+/**
+ * La clase Jedi contiene uno de los personajes con sus atributos, el cual se
+ * enfrenta a otro creado en otra clase llamada Sith.
+ * 
+ * @author matia
+ * @version 1.0
+ * @since 27/03/2024
+ */
 
 public class Jedi {
 	// ATRIBUTOS
@@ -11,6 +21,14 @@ public class Jedi {
 	private Random random;
 	private String tipoCapa;
 
+	/**
+	 * Parametros necesarios para la creacion del objeto Jedi.
+	 * 
+	 * @param nombre
+	 * @param salud
+	 * @param fuerza
+	 * @param tipoCapa
+	 */
 	// CONSTRUCTOR
 	public Jedi(String nombre, int salud, int fuerza, String tipoCapa) {
 		super();
@@ -21,37 +39,67 @@ public class Jedi {
 		this.tipoCapa = tipoCapa;
 	}
 
+	/**
+	 * 
+	 * @return Permite obtener el contenido de la variable nombre.
+	 */
 	// CREO GETTER AND SETTER
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * 
+	 * @param nombre Permite modificar el atributo nombre.
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * 
+	 * @return Permite obtener el contenido de la variable salud.
+	 */
 	public int getSalud() {
 		return salud;
 	}
 
+	/**
+	 * 
+	 * @param salud. Permite modificar el atributo salud.
+	 */
 	public void setSalud(int salud) {
 		this.salud = salud;
 	}
 
+	/**
+	 * 
+	 * @return Permite obtener el contenido de la variable fuerza.
+	 */
 	public int getFuerza() {
 		return fuerza;
 	}
 
+	/**
+	 * 
+	 * @param fuerza. Permite modificar el atributo fuerza.
+	 */
 	public void setFuerza(int fuerza) {
 		this.fuerza = fuerza;
 	}
 
+	/**
+	 * Metodo toString, permite ver los datos del objeto Sith.
+	 */
 	// METODO TOSTRING
 	@Override
 	public String toString() {
 		return "Jedi [nombre=" + nombre + ", salud=" + salud + ", fuerza=" + fuerza + ", tipoCapa=" + tipoCapa + "]";
 	}
 
+	/**
+	 * El metodo imprimir, muestra por pantalla todos los datos de el objeto Jedi.
+	 */
 	// METODO IMPIRMIR
 	public void imprimir() {
 		System.out.println("Nombre: " + this.nombre);
@@ -60,13 +108,59 @@ public class Jedi {
 		System.out.println("Tipo de capa: " + this.tipoCapa);
 	}
 
+	/**
+	 * Metodo importante ya que es el que interactua con ambos objetos(personajes).
+	 * 
+	 * @param sith. Este parámetro es un objeto Sith al que el Jedi le infringe un
+	 *              daño.
+	 * @deprecated Creado metodo nuevo, mejorado llamado atacarNuevo.
+	 */
 	// METODO ATACAR
 	public void atacar(Sith sith) {
 		int danyo = random.nextInt(30, 60);
 		System.out.println(this.nombre + " ataca a " + sith.getNombre() + "!");
 		sith.recibirAtaque(danyo);
 	}
+	
+	/**
+	 * 
+	 * @param sith. Este parámetro es un objeto Sith al que el Jedi le infringe un
+	 *              daño, mejora el antiguo metodo atacar, esta desde la version 1.2.
+	 *              @since 1.2.
+	 */
+	public void atacarNuevo(Sith sith) {
+	    int danyo = calcularDanyo();
+	    System.out.println(this.nombre + " ataca a " + sith.getNombre() + "!");
+	    if (esGolpeCritico()) {
+	        danyo += 20; // Daño adicional por golpe crítico
+	        System.out.println("¡Golpe crítico!");
+	    }
+	    sith.recibirAtaque(danyo);
+	}
 
+	/**
+	 * Este metodo calcula el daño.
+	 * 
+	 * @return Devuelve daño aleatorio entre 30 y 60.
+	 */
+	private int calcularDanyo() {
+	    return random.nextInt(31) + 30;
+	}
+
+	/**
+	 * Este metodo gestiona la probabilidad de un golpe crítico.
+	 * @return Devuelve la probabilidad de 20% de golpe crítico.
+	 */
+	private boolean esGolpeCritico() {
+	    return random.nextDouble() < 0.2;
+	}
+
+
+	/**
+	 * 
+	 * @param danyo. Este parámetro recoje el daño recibido en el ataque del Sith
+	 *               hacia el objeto Jedi.
+	 */
 	// METODO RECIBIR ATAQUE
 	public void recibirAtaque(int danyo) {
 		this.salud -= danyo;
@@ -77,6 +171,10 @@ public class Jedi {
 		}
 	}
 
+	/**
+	 * El metodo grito de guerra, en el caso de ganar la pelea es enseñado en
+	 * pantalla.
+	 */
 	// METODO GRITO GUERRA
 	public void gritoGuerra() {
 		System.out.println("Que la fuerza te acompañe!!");
--- PracticaEntornosS_Santa/src/starwars/Sith.java
+++ PracticaEntornosS_Santa/src/starwars/Sith.java
@@ -3,6 +3,16 @@ package starwars;
 import java.util.Objects;
 import java.util.Random;
 
+/**
+ * 
+ * La clase Sith contiene uno de los personajes con sus atributos, el cual se
+ * enfrenta a otro creado en otra clase llamada Jedi
+ * 
+ * @author matia
+ * @version 1.0
+ * @since 27/03/2024
+ */
+
 public class Sith {
 
 	// ATRIBUTOS
@@ -12,6 +22,14 @@ public class Sith {
 	private Random random;
 	private String tipoEspada;
 
+	/**
+	 * Parametros necesarios para la creacion del objeto Sith.
+	 * 
+	 * @param nombre
+	 * @param salud
+	 * @param fuerza
+	 * @param tipoEspada
+	 */
 	// CONSTRUCTOR
 	public Sith(String nombre, int salud, int fuerza, String tipoEspada) {
 		super();
@@ -22,31 +40,58 @@ public class Sith {
 		this.tipoEspada = tipoEspada;
 	}
 
+	/**
+	 * 
+	 * @return Permite obtener el contenido de la variable nombre.
+	 */
 	// CREO GETTER AND SETTER
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * 
+	 * @param nombre. Permite modificar el atributo nombre.
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * 
+	 * @return Permite obtener el contenido de la variable salud.
+	 */
 	public int getSalud() {
 		return salud;
 	}
 
+	/**
+	 * 
+	 * @param salud. Permite modificar el atributo salud.
+	 */
 	public void setSalud(int salud) {
 		this.salud = salud;
 	}
 
+	/**
+	 * 
+	 * @return Permite obtener el contenido de la variable fuerza.
+	 */
 	public int getFuerza() {
 		return fuerza;
 	}
 
+	/**
+	 * 
+	 * @param fuerza. Permite modificar el atributo fuerza.
+	 */
 	public void setFuerza(int fuerza) {
 		this.fuerza = fuerza;
 	}
 
+	/**
+	 * Metodo toString, permite ver los datos del objeto Sith.
+	 */
 	// METODO TOSTRING
 	@Override
 	public String toString() {
@@ -54,6 +99,9 @@ public class Sith {
 				+ "]";
 	}
 
+	/**
+	 * El metodo imprimir, muestra por pantalla todos los datos de el objeto Sith.
+	 */
 	// METODO IMPIRMIR
 	public void imprimir() {
 		System.out.println("Nombre: " + this.nombre);
@@ -62,6 +110,15 @@ public class Sith {
 		System.out.println("Tipo de espada: " + tipoEspada);
 	}
 
+	/**
+	 * Metodo importante ya que es el que interactua con ambos objetos(personajes)
+	 * infringiendo daño con un ataque.
+	 * 
+	 * @param jedi. Este parámetro es un objeto Jedi al que el Sith le infringe un
+	 *              daño.
+	 * @deprecated Deja de funcionar porque hay una version mas actual.
+	 * 
+	 */
 	// METODO ATACAR
 	public void atacar(Jedi jedi) {
 		int danyo = random.nextInt(30, 60);
@@ -69,6 +126,48 @@ public class Sith {
 		jedi.recibirAtaque(danyo);
 	}
 
+	/**
+	 * 
+	 * Metodo mejorado de para generar ataque, infringe daño con un ataque.
+	 * 
+	 * @param jedi. Este parámetro es un objeto Jedi al que el Sith le infringe un
+	 *              daño.
+	 */
+	public void atacarNuevo(Jedi jedi) {
+		int danyo = calcularDanyo();
+		System.out.println(this.nombre + " ataca a " + jedi.getNombre() + "!");
+		if (esGolpeCritico()) {
+			danyo += 20; // Daño adicional por golpe crítico
+			System.out.println("¡Golpe crítico!");
+		}
+		jedi.recibirAtaque(danyo);
+	}
+
+	/**
+	 * 
+	 * Este metodo calcula el daño.
+	 * 
+	 * @return Devuelve daño aleatorio entre 30 y 60.
+	 */
+	private int calcularDanyo() {
+		return random.nextInt(31) + 30; // Daño aleatorio entre 30 y 60
+	}
+
+	/**
+	 * 
+	 * Este metodo gestiona la probabilidad de un golpe crítico.
+	 * 
+	 * @return Devuelve la probabilidad de 20% de golpe crítico.
+	 */
+	private boolean esGolpeCritico() {
+		return random.nextDouble() < 0.2; // Probabilidad de 20% de golpe crítico
+	}
+
+	/**
+	 * 
+	 * @param danyo. Este parámetro recoje el daño recibido en el ataque del Jedi
+	 *               hacia el objeto Sith.
+	 */
 	// METODO RECIBIR ATAQUE
 	public void recibirAtaque(int danyo) {
 		this.salud -= danyo;
@@ -79,6 +178,10 @@ public class Sith {
 		}
 	}
 
+	/**
+	 * El metodo amenaza oscura, en el caso de ganar la pelea es enseñado en
+	 * pantalla.
+	 */
 	// METODO AMENAZA OSCURA
 	public void amenazaOscura() {
 		System.out.println("Has muerto Jedi...");

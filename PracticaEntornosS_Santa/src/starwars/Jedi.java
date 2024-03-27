eb262a1c3ab6aa2e6d0a56fed6f9084714367c2c
Matias Pencef <matiaspencef@gmail.com>
2024-03-27 15:43:59 +0100

    Añadí todos los metodos

--- PracticaEntornosS_Santa/src/starwars/Jedi.java
+++ PracticaEntornosS_Santa/src/starwars/Jedi.java
@@ -9,14 +9,16 @@ public class Jedi {
 	private int salud;
 	private int fuerza;
 	private Random random;
+	private String tipoCapa;
 
 	// CONSTRUCTOR
-	public Jedi(String nombre, int salud, int fuerza, Random random) {
+	public Jedi(String nombre, int salud, int fuerza, String tipoCapa) {
 		super();
 		this.nombre = nombre;
 		this.salud = salud;
 		this.fuerza = fuerza;
-		this.random = random;
+		this.random = new Random();
+		this.tipoCapa = tipoCapa;
 	}
 
 	// CREO GETTER AND SETTER
@@ -42,6 +44,42 @@ public class Jedi {
 
 	public void setFuerza(int fuerza) {
 		this.fuerza = fuerza;
+	}
+
+	// METODO TOSTRING
+	@Override
+	public String toString() {
+		return "Jedi [nombre=" + nombre + ", salud=" + salud + ", fuerza=" + fuerza + ", tipoCapa=" + tipoCapa + "]";
+	}
+
+	// METODO IMPIRMIR
+	public void imprimir() {
+		System.out.println("Nombre: " + this.nombre);
+		System.out.println("Salud: " + this.salud);
+		System.out.println("Fuerza: " + this.fuerza);
+		System.out.println("Tipo de capa: " + this.tipoCapa);
+	}
+
+	// METODO ATACAR
+	public void atacar(Sith sith) {
+		int danyo = random.nextInt(30, 60);
+		System.out.println(this.nombre + " ataca a " + sith.getNombre() + "!");
+		sith.recibirAtaque(danyo);
+	}
+
+	// METODO RECIBIR ATAQUE
+	public void recibirAtaque(int danyo) {
+		this.salud -= danyo;
+		if (this.salud <= 0) {
+			System.out.println(this.nombre + " ha sido derrotado.");
+		} else {
+			System.out.println(this.nombre + " recibe " + danyo + " puntos de daño. Salud restante: " + this.salud);
+		}
+	}
+
+	// METODO GRITO GUERRA
+	public void gritoGuerra() {
+		System.out.println("Que la fuerza te acompañe!!");
 	}
 
 }
--- PracticaEntornosS_Santa/src/starwars/Sith.java
+++ PracticaEntornosS_Santa/src/starwars/Sith.java
@@ -10,14 +10,16 @@ public class Sith {
 	private int salud;
 	private int fuerza;
 	private Random random;
+	private String tipoEspada;
 
 	// CONSTRUCTOR
-	public Sith(String nombre, int salud, int fuerza, Random random) {
+	public Sith(String nombre, int salud, int fuerza, String tipoEspada) {
 		super();
 		this.nombre = nombre;
 		this.salud = salud;
 		this.fuerza = fuerza;
-		this.random = random;
+		this.random = new Random();
+		this.tipoEspada = tipoEspada;
 	}
 
 	// CREO GETTER AND SETTER
@@ -43,6 +45,43 @@ public class Sith {
 
 	public void setFuerza(int fuerza) {
 		this.fuerza = fuerza;
+	}
+
+	// METODO TOSTRING
+	@Override
+	public String toString() {
+		return "Sith [nombre=" + nombre + ", salud=" + salud + ", fuerza=" + fuerza + ", tipoEspada=" + tipoEspada
+				+ "]";
+	}
+
+	// METODO IMPIRMIR
+	public void imprimir() {
+		System.out.println("Nombre: " + this.nombre);
+		System.out.println("Salud: " + this.salud);
+		System.out.println("Fuerza: " + this.fuerza);
+		System.out.println("Tipo de espada: " + tipoEspada);
+	}
+
+	// METODO ATACAR
+	public void atacar(Jedi jedi) {
+		int danyo = random.nextInt(30, 60);
+		System.out.println(this.nombre + " ataca a " + jedi.getNombre() + "!");
+		jedi.recibirAtaque(danyo);
+	}
+
+	// METODO RECIBIR ATAQUE
+	public void recibirAtaque(int danyo) {
+		this.salud -= danyo;
+		if (this.salud <= 0) {
+			System.out.println(this.nombre + " ha sido derrotado.");
+		} else {
+			System.out.println(this.nombre + " recibe " + danyo + " puntos de daño. Salud restante: " + this.salud);
+		}
+	}
+
+	// METODO AMENAZA OSCURA
+	public void amenazaOscura() {
+		System.out.println("Has muerto Jedi...");
 	}
 
 }

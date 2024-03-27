30c7f701c3d0279c366b5185beeb6989f933a109
Matias Pencef <matiaspencef@gmail.com>
2024-03-27 14:52:57 +0100

    He creado atributos, constructores y get y set.

--- PracticaEntornosS_Santa/src/starwars/Jedi.java
+++ PracticaEntornosS_Santa/src/starwars/Jedi.java
@@ -1,5 +1,47 @@
 package starwars;
 
+import java.util.Objects;
+import java.util.Random;
+
 public class Jedi {
+	// ATRIBUTOS
+	private String nombre;
+	private int salud;
+	private int fuerza;
+	private Random random;
+
+	// CONSTRUCTOR
+	public Jedi(String nombre, int salud, int fuerza, Random random) {
+		super();
+		this.nombre = nombre;
+		this.salud = salud;
+		this.fuerza = fuerza;
+		this.random = random;
+	}
+
+	// CREO GETTER AND SETTER
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getSalud() {
+		return salud;
+	}
+
+	public void setSalud(int salud) {
+		this.salud = salud;
+	}
+
+	public int getFuerza() {
+		return fuerza;
+	}
+
+	public void setFuerza(int fuerza) {
+		this.fuerza = fuerza;
+	}
 
 }
--- PracticaEntornosS_Santa/src/starwars/Sith.java
+++ PracticaEntornosS_Santa/src/starwars/Sith.java
@@ -1,5 +1,48 @@
 package starwars;
 
+import java.util.Objects;
+import java.util.Random;
+
 public class Sith {
+
+	// ATRIBUTOS
+	private String nombre;
+	private int salud;
+	private int fuerza;
+	private Random random;
+
+	// CONSTRUCTOR
+	public Sith(String nombre, int salud, int fuerza, Random random) {
+		super();
+		this.nombre = nombre;
+		this.salud = salud;
+		this.fuerza = fuerza;
+		this.random = random;
+	}
+
+	// CREO GETTER AND SETTER
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getSalud() {
+		return salud;
+	}
+
+	public void setSalud(int salud) {
+		this.salud = salud;
+	}
+
+	public int getFuerza() {
+		return fuerza;
+	}
+
+	public void setFuerza(int fuerza) {
+		this.fuerza = fuerza;
+	}
 
 }
